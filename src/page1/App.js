c4a1a9915ae5ff607d45bc847cd244443a1ffca9
xuw-i(10027568) <xuw-i@glodon.com>
2020-12-30 10:15:54 +0800

    初始化react多页面

---
+++ src/page1/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to App11</h2>
+        </div>
+      </div>
+    );
+  }
+  aaa:['sele','input']
+}
+ 
+export default App;
